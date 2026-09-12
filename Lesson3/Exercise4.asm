bca996fa8300da5d5b301e31464fcbf7c389090b
Aaron Ludwig Altar <aaronaltar@gmail.com>
2026-09-12 17:24:28 +0800

    using loop and incrementation

---
+++ Lesson3/Exercise4.asm
@@ -0,0 +1,30 @@
+; AARON LUDWIG A. ALTAR
+; CPE4B
+
+.model small
+.stack
+.code
+
+start:
+        mov ah, 02h
+        mov cx, 26        ; 26 letters, A to Z
+        mov bl, 61h       ; bl = 'a'
+        mov dl, 41h       ; dl = 'A'
+        mov dh, dl        ; dh = 'A'
+back:
+        int 21h           ; display uppercase (dl)
+        mov dl, bl        ; load lowercase
+        int 21h           ; display lowercase
+        inc dh            ; next uppercase letter
+        inc bl            ; next lowercase letter 
+        mov dl, dh
+        loop back
+
+        mov dl, 0dh     
+        int 21h
+        mov dl, 0ah	     
+        int 21h
+
+        mov ah, 4ch
+        int 21h
+end start
