180baa045a104a01a4b929b9c2b72c88c3e2d852
Aaron Ludwig Altar <aaronaltar@gmail.com>
2026-09-16 08:13:53 +0800

    added exercise 6

---
+++ Lesson3/Exer6.asm
@@ -0,0 +1,38 @@
+; AARON LUDWIG A. ALTAR
+; CPE4B
+
+.model small
+.stack
+.code
+
+start:
+        mov ah, 02h
+        mov cx, 26        ; 26 letters, A to Z
+
+        mov bl, 5ah       ; bl = 'Z'
+        mov dl, 61h       ; dl = 'a'
+        mov dh, dl        ; dh = 'a'
+back:
+        int 21h           ; display uppercase (dl)
+        mov dl, bl        ; load lowercase
+        int 21h           ; display lowercase
+
+        mov dl, 0dh     
+        int 21h
+        mov dl, 0ah	     
+        int 21h
+ 
+        dec bl            ; next uppercase letter
+        inc dh            ; next lowercase letter 
+        
+        mov dl, dh
+        loop back
+
+        mov dl, 0dh     
+        int 21h
+        mov dl, 0ah	     
+        int 21h
+
+        mov ah, 4ch
+        int 21h
+end start
